d046d8261d4417606bf666fa781c21bfcd73223a
francescosileoni <francesco.sile@gmail.com>
2024-04-22 16:53:58 +0200

    creazione dao provincia,indirizzo,comune

---
+++ src/main/java/Team2/BuildWeek3/repositories/ProvinciaDAO.java
@@ -0,0 +1,9 @@
+package Team2.BuildWeek3.repositories;
+
+import Team2.BuildWeek3.entities.Provincia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinciaDAO extends JpaRepository<Provincia,Integer> {
+
+
+}
