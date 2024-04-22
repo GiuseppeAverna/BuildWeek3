718b55be5d880e857c1edf1280cb29329a7ae40a
GiuseppeAverna <151162626+GiuseppeAverna@users.noreply.github.com>
2024-04-22 11:19:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BuildWeek3
+ 
---
+++ src/test/java/Team2/BuildWeek3/BuildWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Team2.BuildWeek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeek3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
