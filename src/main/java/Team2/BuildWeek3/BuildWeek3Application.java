73cf1791992c907c686fd1416a9e2a40508c5daf
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-04-26 12:56:08 +0200

    fix

--- src/main/java/Team2/BuildWeek3/BuildWeek3Application.java
+++ src/main/java/Team2/BuildWeek3/BuildWeek3Application.java
@@ -1,13 +1,18 @@
-//package Team2.BuildWeek3;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class BuildWeek3Application {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(BuildWeek3Application.class, args);
-//	}
-//
-//}
+package Team2.BuildWeek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class BuildWeek3Application {
+
+	public static void main(String[] args) {
+        SpringApplication.run(BuildWeek3Application.class, args);
+//        ConfigurableApplicationContext context = SpringApplication.run(BuildWeek3Application.class, args);
+//        FillDatabaseProvince fillDatabaseProvince = context.getBean(FillDatabaseProvince.class);
+//        fillDatabaseProvince.fillDatabase();
+//        fillDatabaseProvince.fillDatabaseComuni();
+	}
+
+}
